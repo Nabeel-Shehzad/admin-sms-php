7a6b5fdbf311c9d6f1e4bf017bdb140b9273aafa
Nabeel-Shehzad <NabeeShahzad88@gmail.com>
2022-09-11 01:52:25 +0500

    remove bug

--- home.php
+++ home.php
@@ -73,6 +73,7 @@ if (!isset($_SESSION['username'])) {
                     <th>SMS ID</th>
                     <th>Contact</th>
                     <th>Date</th>
+                    <th>Model</th>
                     <th>Action</th>
                 </tr>
                 </tfoot>
